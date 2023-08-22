8a9cabfd8addb1b6a9a22c9ad5462fc8c3d39b31
Kristen Moore <kookwithkris@yahoo.com>
2023-08-22 14:13:01 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class Cupcake {
+
+        public double price;
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public void type() {
+            System.out.println("A basic, generic cupcake, with vanilla frosting");
+        }
+
+        public class RedVelvet {
+
+        }
+
+
+    }
+}
+
